5890fc3f75ab230d606700eab20730f654911b47
Janos Wolosz <janos.wolosz@gmail.com>
2025-10-11 08:10:27 +0200

    Add problem

---
+++ Archive/Imo/komal.lean
@@ -0,0 +1,22 @@
+/-
+Copyright (c) 2024 Joseph Myers. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Myers
+-/
+import Mathlib.Data.Rat.Floor
+import Mathlib.SetTheory.Cardinal.Basic
+
+/-!
+# Komal problem
+Let `a` and `b` positive integers, so that for any `n` positive integer `a^n + n ∣ b^n + n`.
+Show that `a = b`.
+-/
+
+namespace Komal
+
+/-- The answer 2 is to be determined by the solver of the original problem. -/
+theorem _root_.komal :
+    ∀ a b : ℕ+, (∀ n : ℕ+, ((a : ℕ) ^ (n : ℕ) + n ∣ (b : ℕ) ^ (n : ℕ) + n)) → a = b := by
+  sorry
+
+end Komal
