ff8bb2afcca83cecef4e2e5d5e25245bab4fa2bf
Janos Wolosz <janos.wolosz@gmail.com>
2026-01-10 08:41:11 +0100

    Steps forward

--- Mathlib/Algebra/Lie/Weights/Main.lean
+++ Mathlib/Algebra/Lie/Weights/Main.lean
@@ -9,6 +9,19 @@ open LieAlgebra LieModule Module
 variable {H : LieSubalgebra K L} [H.IsCartanSubalgebra]
 variable [IsKilling K L] [IsTriangularizable K H L]
 
+lemma exists_rootSet_lieIdeal_eq (I : LieIdeal K L) :
+    ∃ Φ : Set H.root, I.toSubmodule = (I.toSubmodule ⊓ H.toSubmodule) ⊔
+      ⨆ α ∈ Φ, (rootSpace H α.1).toSubmodule := by
+  admit
+
+theorem isCompl_killingCompl (I : LieIdeal K L) :
+    IsCompl I I.killingCompl := by
+  admit
+
+theorem compl_eq_killingCompl (I : LieIdeal K L) :
+    Iᶜ = I.killingCompl := by
+  admit
+
 section IsIrreducible
 
 variable [(rootSystem H).IsIrreducible]
