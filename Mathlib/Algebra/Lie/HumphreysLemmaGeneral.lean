e01c5d3d1b85b80731f35320a3bdb2db4612a7fe
Janos Wolosz <janos.wolosz@gmail.com>
2026-02-11 08:16:48 +0100

    Fix date

--- Mathlib/Algebra/Lie/HumphreysLemmaGeneral.lean
+++ Mathlib/Algebra/Lie/HumphreysLemmaGeneral.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Janos Wolosz. All rights reserved.
+Copyright (c) 2026 Janos Wolosz. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Janos Wolosz
 -/
