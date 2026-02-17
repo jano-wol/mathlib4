a4c518a69adadfdc22e89543a09d57edd8630235
jano-wol <78079945+jano-wol@users.noreply.github.com>
2026-02-17 06:40:54 +0100

    Take over reviewer findings
    Co-authored-by: Oliver Nash <7734364+ocfnash@users.noreply.github.com>

--- Mathlib/Algebra/Lie/Killing.lean
+++ Mathlib/Algebra/Lie/Killing.lean
@@ -130,29 +130,22 @@ end LieEquiv
 
 end LieAlgebra
 
-namespace LieIdeal
-
-variable {K L} [LieAlgebra.IsKilling K L] [Module.Finite K L]
-
-lemma disjoint_killingCompl (I : LieIdeal K L) : Disjoint I I.killingCompl := by
-  rw [disjoint_iff]
-  suffices IsLieAbelian (I ⊓ I.killingCompl : LieIdeal K L) from
-    LieAlgebra.IsKilling.ideal_eq_bot_of_isLieAbelian _
-  rw [LieSubmodule.lie_abelian_iff_lie_self_eq_bot, LieSubmodule.lie_eq_bot_iff]
-  intro x ⟨_, hx⟩ y ⟨hy, _⟩
-  suffices h : ∀ z, (LieModule.traceForm K L L) ⁅x, y⁆ z = 0 from
-    (LieAlgebra.IsKilling.killingForm_nondegenerate K L).1 _ h
-  intro z
+open LieAlgebra in
+variable {K L} in
+lemma LieIdeal.isCompl_killingCompl [IsKilling K L] [Module.Finite K L] (I : LieIdeal K L) :
+    IsCompl I I.killingCompl := by
+  suffices Disjoint I I.killingCompl by
+    rwa [← LieSubmodule.isCompl_toSubmodule, I.toSubmodule_killingCompl,
+      LinearMap.BilinForm.isCompl_orthogonal_iff_disjoint (LieModule.traceForm_isSymm K L L).isRefl,
+      ← I.toSubmodule_killingCompl, LieSubmodule.disjoint_toSubmodule]
+  suffices IsLieAbelian (I ⊓ I.killingCompl : LieIdeal K L) by
+    rw [disjoint_iff]
+    exact IsKilling.ideal_eq_bot_of_isLieAbelian _
+  suffices ∀ (x y z : L) (hx : x ∈ killingCompl K L I) (hy : y ∈ I),
+      LieModule.traceForm K L L ⁅x, y⁆ z = 0 by
+    rw [LieSubmodule.lie_abelian_iff_lie_self_eq_bot, LieSubmodule.lie_eq_bot_iff]
+    rintro x ⟨-, hx⟩ y ⟨hy, -⟩
+    exact (IsKilling.killingForm_nondegenerate K L).1 _ fun z ↦ this x y z hx hy
+  intro x y z hx hy
   rw [LieModule.traceForm_apply_lie_apply K L L x y z, LieModule.traceForm_comm K L L]
-  exact I.mem_killingCompl.mp hx _ (lie_mem_left K L I y z hy)
-
-lemma isCompl_killingCompl (I : LieIdeal K L) : IsCompl I I.killingCompl := by
-  rw [← LieSubmodule.isCompl_toSubmodule, I.toSubmodule_killingCompl]
-  exact (LinearMap.BilinForm.isCompl_orthogonal_iff_disjoint
-    (LieModule.traceForm_isSymm K L L).isRefl).mpr
-    (I.toSubmodule_killingCompl ▸ LieSubmodule.disjoint_toSubmodule.mpr (disjoint_killingCompl I))
-
-lemma compl_eq_killingCompl (I : LieIdeal K L) : Iᶜ = I.killingCompl :=
-  IsCompl.compl_eq (isCompl_killingCompl I)
-
-end LieIdeal
+  exact I.mem_killingCompl.mp hx _ <| lie_mem_left K L I y z hy
